fca4a45a982a0a2575c586b05574b8f56c478d5f
Sato Hiroaki <satohiroaki88@gmail.com>
2021-02-20 22:31:20 +0900

    feature: video-objet,audio-object

--- tags/media-tag.js
+++ tags/media-tag.js
@@ -8,10 +8,42 @@ export class ImageObject extends DefineObject
         if(this.obj)
         {
             this.img = document.createElement('img');
-            this.img.setAttribute('src',`${this.obj}`);
+            if(typeof this.obj === 'string') this.img.setAttribute('src',`${this.obj}`);
+            else this.img.setAttribute('src',URL.createObjectURL(this.obj));
+            this.appendChild(this.img);
+        }
+    }
+}
+
+export class VideoObject extends DefineObject
+{
+    async onPulled()
+    {
+        this.innerHTML = '';
+        if(this.obj)
+        {
+            this.img = document.createElement('video');
+            if(typeof this.obj === 'string') this.img.setAttribute('src',`${this.obj}`);
+            else this.img.setAttribute('src',URL.createObjectURL(this.obj));
+            this.appendChild(this.img);
+        }
+    }
+}
+export class AudioObject extends DefineObject
+{
+    async onPulled()
+    {
+        this.innerHTML = '';
+        if(this.obj)
+        {
+            this.img = document.createElement('audio');
+            if(typeof this.obj === 'string') this.img.setAttribute('src',`${this.obj}`);
+            else this.img.setAttribute('src',URL.createObjectURL(this.obj));
             this.appendChild(this.img);
         }
     }
 }
 
 customElements.define('image-object',ImageObject);
+customElements.define('video-object',VideoObject);
+customElements.define('autio-object',AudioObject);
